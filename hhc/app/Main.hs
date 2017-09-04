ccc069cee9e4fd660cf06ab2b95b822c20e47a38
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-04 22:49:06 +0200

    Add param validation, help info stub

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -5,7 +5,15 @@ import System.Directory
 import System.IO
 import Lib
 
+validateArgs :: [String] -> IO ()
+validateArgs args =
+    let incorrectUsage = putStrLn "incorrect usage\nusage charset minLen maxLen hash" in
+    let processing = putStrLn "processing, wait...\n" in
+    let argsLen = length args in
+      if (argsLen < 4 || argsLen > 4) then incorrectUsage else processing
+
 main :: IO ()
 main = do
     args <- getArgs
+    validateArgs args    
     print $ sha1bf ['a'..'z'] 1 5 "b40981aab75932c5b2f555f50769d878e44913d7"
