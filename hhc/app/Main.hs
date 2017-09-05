d778e8452d4a58c1e29c91610fbdf8bd56f9202c
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-05 22:20:36 +0200

    Exit when run with invalid number of parameters

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Environment
 import System.Directory
+import System.Exit
 import System.IO
 import Lib
 
@@ -10,10 +11,16 @@ validateArgs args =
     let incorrectUsage = putStrLn "incorrect usage\nusage charset minLen maxLen hash" in
     let processing = putStrLn "processing, wait...\n" in
     let argsLen = length args in
-      if (argsLen < 4 || argsLen > 4) then incorrectUsage else processing
+      if (argsLen < 4 || argsLen > 4) then do
+        incorrectUsage
+        exitFailure
+      else do
+          processing
+
 
 main :: IO ()
 main = do
     args <- getArgs
-    validateArgs args    
+    validateArgs args
+
     print $ sha1bf ['a'..'z'] 1 5 "b40981aab75932c5b2f555f50769d878e44913d7"
