aa50bdcdbc3c50f635aaf20d1d61db1853a4fb71
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-08 17:17:59 +0200

    Introduce Charset module

---
+++ hhc/src/Charset.hs
@@ -0,0 +1,22 @@
+module Charset
+    ( genplain,
+    lowerLimit,
+    upperLimit
+    ) where
+
+import           Data.List
+
+-- charset, index -> plain text
+genplain :: String -> Int -> String
+genplain c i =
+  if (i < length c) then getchar else prefix ++ getchar
+    where
+        qr = i `divMod` length c
+        prefix = genplain c ((fst qr) - 1)
+        getchar = c !! (snd qr):[]
+
+lowerLimit :: Int -> Int -> Int
+lowerLimit charsetLen plainLen = sum [ charsetLen ^ x | x <- [0..plainLen - 1]] - 1
+
+upperLimit :: Int -> Int -> Int
+upperLimit charsetLen plainLen = sum [ charsetLen ^ x | x <- [0..plainLen]]
--- hhc/src/Lib.hs
+++ hhc/src/Lib.hs
@@ -8,21 +8,7 @@ import qualified Data.ByteString.Lazy.Char8 as C
 import           Data.Digest.Pure.SHA
 import           Data.List
 import           Data.Maybe.Utils
-
--- charset, index -> plain text
-genplain :: String -> Int -> String
-genplain c i =
-  if (i < length c) then getchar else prefix ++ getchar
-    where
-        qr = i `divMod` length c
-        prefix = genplain c ((fst qr) - 1)
-        getchar = c !! (snd qr):[]
-
-lowerLimit :: Int -> Int -> Int
-lowerLimit charsetLen plainLen = sum [ charsetLen ^ x | x <- [0..plainLen - 1]] - 1
-
-upperLimit :: Int -> Int -> Int
-upperLimit charsetLen plainLen = sum [ charsetLen ^ x | x <- [0..plainLen]]
+import           Charset
 
 -- charset, min len, max len, cipher -> plain text
 sha1bf :: String -> Int -> Int -> String -> (String, String)
