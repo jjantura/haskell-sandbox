a234bd5492498f09b5bc4a950fd8d191ed0e223a
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-12 17:07:59 +0200

    Use fold for result aggregation

--- hhc/src/Lib.hs
+++ hhc/src/Lib.hs
@@ -12,11 +12,17 @@ import           Charset
 
 -- TODO: operate on hash list instead of single hash (performance)
 -- charset, min len, max len, cipher -> (hash, maybe plain text)
-sha1bf :: String -> Int -> Int -> String -> (String, String)
+sha1bf :: String -> Int -> Int -> String -> [(String, String)]
 sha1bf charset minl maxl hash =
     let
       plains = [genplain charset i | i <- [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]]
       hashes = map showDigest $ map sha1 $ map C.pack plains
       maybeIndex = elemIndex hash hashes
     in
-      if isJust maybeIndex then (hash,  genplain charset (fromJust maybeIndex + (lowerLimit (length charset) minl))) else (hash, "")
+      foldl (\a e -> if isJust maybeIndex then [(hash, genplain charset (fromJust maybeIndex + (lowerLimit (length charset) minl)))] else []) [] plains
+    -- let
+    --   plains = [genplain charset i | i <- [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]]
+    --   hashes = map showDigest $ map sha1 $ map C.pack plains
+    --   maybeIndex = elemIndex hash hashes
+    -- in
+    --   if isJust maybeIndex then (hash,  genplain charset (fromJust maybeIndex + (lowerLimit (length charset) minl))) else (hash, "")
