129b396b5dd694ee2c5f99e12710c785e421ad72
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-17 22:50:46 +0200

    Replace foldl with foldl'

--- hhc/src/Lib.hs
+++ hhc/src/Lib.hs
@@ -14,7 +14,7 @@ sha1bf :: String -> Int -> Int -> [String] -> [(String, String)]
 sha1bf charset minl maxl hashes =
   let
     digest e = showDigest $ sha1 $ C.pack $ genPlain charset e
-    foundIndices = foldl (\a e -> if isJust (elemIndex (digest e) hashes) then e:a else a) [] [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]
+    foundIndices = foldl' (\a e -> if isJust (elemIndex (digest e) hashes) then e:a else a) [] [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]
     plains = map (genPlain charset) foundIndices
   in
     zip plains $ map showDigest $ map sha1 $ map C.pack $ plains
