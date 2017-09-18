ad866fee37699e1bbb53b61bd9d68e2d0cb0632e
Jaroslaw Jantura <jjantura@gmail.com>
2017-09-18 23:41:26 +0200

    Introduce cryptonite

--- hhc/src/Bruteforce.hs
+++ hhc/src/Bruteforce.hs
@@ -3,18 +3,21 @@ module Bruteforce
     ( sha1bf
     ) where
 
-import qualified Data.ByteString.Lazy.Char8 as C
-import           Data.Digest.Pure.SHA
+import           Crypto.Hash             (hashWith, SHA1 (..))
 import           Data.List
 import           Data.Maybe
+import           Data.Text               (pack)
+import           Data.Text.Encoding      (encodeUtf8)
 import           Charset
+
 
 -- charset, min len, max len, cipher -> (hash, maybe plain text)
 sha1bf :: String -> Int -> Int -> [String] -> [(String, String)]
 sha1bf charset minl maxl hashes =
   let
-    digest e = showDigest $ sha1 $ C.pack $ genPlain charset e
+    plainToDigest e = show $ hashWith SHA1 $ encodeUtf8 $ pack e
+    digest e = plainToDigest $ genPlain charset e
     foundIndices = foldl' (\a e -> if isJust (elemIndex (digest e) hashes) then e:a else a) [] [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]
     plains = map (genPlain charset) foundIndices
   in
-    zip plains $ map showDigest $ map sha1 $ map C.pack $ plains
+    zip plains $ map plainToDigest plains
