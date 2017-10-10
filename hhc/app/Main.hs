302f0d7ee107e14010e4a968bbf18839e1ddadc2
Jaroslaw Jantura <jjantura@gmail.com>
2017-10-10 23:06:31 +0200

    Add custom args parsing...

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -6,47 +6,66 @@ import System.Directory
 import System.Exit
 import System.IO
 import System.Clock
+import Data.List
+import Data.Maybe
 
 import Formatting
 import Formatting.Clock
 
 import Control.Exception
 import Control.Monad
-import           Crypto.Hash             (hashWith, SHA1 (..))
+import Crypto.Hash             (hashWith, SHA1 (..))
 
 import Bruteforce
 import Dictionary
 
-validateArgs :: [String] -> IO ()
-validateArgs args =
-    let incorrectUsage = putStrLn "incorrect usage\nusage charset minLen maxLen file_with_hashes" in
-    let processing = putStrLn "processing, wait...\n" in
-    let argsLen = length args in
-      if argsLen < 4 || argsLen > 4 then do
-        incorrectUsage
-        exitFailure
-      else processing
+takeArgValue :: String -> [String] -> Maybe String
+takeArgValue arg args = 
+    let maybeArgIndex = arg `elemIndex` args in
+    let index = fromJust maybeArgIndex in
+    let argsCount = length args in   
+        if isJust maybeArgIndex then 
+            if index < argsCount then Just (args !! succ index) else Nothing
+        else Nothing
 
+dispatch :: Maybe String -> IO()
+dispatch maybeArg = do    
+    let incorrectUsage = putStrLn "incorrect usage\n" 
+        processing = putStrLn "processing, wait...\n" in
+            if isJust maybeArg then do
+                processing
+                exitSuccess                       
+            else do
+                incorrectUsage
+                exitFailure
+
+        
+-- TODO: this code is ugly and unreadable, replace with applicative optparse asap
+validateArgs :: [String] -> IO ()
+validateArgs args = dispatch $ takeArgValue "-m" args
+    
 main :: IO ()
 main = do
---  TODO: move to method (?), introduce context type (?)
-    hSetEncoding stdout utf8
     args <- getArgs
     validateArgs args
-    let charset = head args
-        minLen = read $ args !! 1 :: Int
-        maxLen = read $ args !! 2 :: Int
-        path = args !! 3
-    -- read file
-    handle <- openFile path ReadMode
-    contents <- hGetContents handle
-    time0 <- getTime Monotonic
+--  TODO: move to method (?), introduce context type (?)
+    -- hSetEncoding stdout utf8
+    -- args <- getArgs
+    -- validateArgs args
+    -- let charset = head args
+    --     minLen = read $ args !! 1 :: Int
+    --     maxLen = read $ args !! 2 :: Int
+    --     path = args !! 3
+    -- -- read file
+    -- handle <- openFile path ReadMode
+    -- contents <- hGetContents handle
+    -- time0 <- getTime Monotonic
 
-    let hashes = lines contents
-        -- plains = bruteforce SHA1 charset minLen maxLen hashes
-        plains = dictionary SHA1 ["ala", "ma", "kota"] hashes
-    print plains
-    time1 <- getTime Monotonic
+    -- let hashes = lines contents
+    --     -- plains = bruteforce SHA1 charset minLen maxLen hashes
+        
+    -- print plains
+    -- time1 <- getTime Monotonic
 
-    let timeDiff = time1 - time0
-    fprint (timeSpecs % "\n") time0 time1
+    -- let timeDiff = time1 - time0
+    -- fprint (timeSpecs % "\n") time0 time1
