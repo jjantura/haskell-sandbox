eb9ddfa771d11b9c8766f96da12e690201824c74
Jaroslaw Jantura <jjantura@gmail.com>
2017-10-16 22:39:29 +0200

    Change argument handlings

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -8,8 +8,10 @@ import System.Exit
 import System.IO
 import System.Clock
 import Data.Char
-import Data.List
+import Data.List as L
+import Data.Map as M
 import Data.Maybe
+import Data.Tuple.Select
 
 import Control.Exception
 import Control.Monad
@@ -21,14 +23,14 @@ import Dictionary
 import File
 
 -- TODO: this code is ugly and unreadable, replace with applicative optparse asap
-takeArgValue :: [String] -> String -> Maybe String
+takeArgValue :: [String] -> String -> (Maybe String, Maybe String)
 takeArgValue args arg = 
     let maybeArgIndex = arg `elemIndex` args in
     let index = fromJust maybeArgIndex in
     let argsCount = length args in   
         if isJust maybeArgIndex then 
-            if index < argsCount then Just (args !! succ index) else Nothing
-        else Nothing
+            if index < argsCount then (Just arg, Just (args !! succ index)) else (Just arg, Nothing)
+        else (Nothing, Nothing)
 
 asInt :: Maybe String -> Int
 asInt s = if isJust s then read $ fromJust s :: Int else -1
@@ -38,27 +40,32 @@ asString s = if isJust s then fromJust s else ""
 
 alg2bf :: String -> String -> Int -> Int -> [String] -> [(String, String)]
 alg2bf alg = 
-    let lowercased = map toLower alg in        
+    let lowercased = L.map toLower alg in        
         case lowercased of
             "sha1" -> bruteforce SHA1
             _ -> bruteforce MD5
 
-
 userBruteforce :: [String] -> IO()
-userBruteforce args = do    
-    let alg = asString $ takeArgValue args "-a" 
-        charset = asString $ takeArgValue args "-c"
-        minLen = asInt $ takeArgValue args "-ll"
-        maxLen = asInt $ takeArgValue args "-ul" in
-        do 
-            content <- loadFile $ asString $ takeArgValue args "-i"             
-            print $ alg2bf alg charset minLen maxLen $ lines content
+userBruteforce args = do            
+        let argMap = M.fromList $ L.map (takeArgValue args) ["-a", "-c", "-ll", "-ul", "-i"]
+            alg = asString $ argMap ! Just "-a"
+            charset = asString $ argMap ! Just "-c"
+            minLen = asInt $ argMap ! Just "-ll"
+            maxLen = asInt $ argMap ! Just "-ul" 
+            input = asString $ argMap ! Just "-i" in
+            do 
+                content <- loadFile input             
+                print $ alg2bf alg charset minLen maxLen $ lines content
 
+--validateBruteforceArgs :: [(Maybe String, Maybe String)] -> IO()           
+-- validateBruteforceArgs argMap = do
+    
+                
 dispatch :: [String] -> IO()
 dispatch args = do    
     let incorrectUsage = putStrLn "incorrect usage\n" 
         processing = putStrLn "processing, wait...\n" 
-        maybeMode = takeArgValue args "-m" in
+        maybeMode = snd $ takeArgValue args "-m" in
             if isJust maybeMode then do
                 case fromJust maybeMode of 
                     "bruteforce" -> userBruteforce args
