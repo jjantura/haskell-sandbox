922729eabb8cff6405daca5a49b403d499614385
Jarosław Jantura <jjantura@gmail.com>
2017-10-29 07:44:59 +0100

    Move benchmark to a separate module

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -19,6 +19,7 @@ import Control.Exception
 import Control.Monad
 import Crypto.Hash             (hashWith, HashAlgorithm(..), SHA1 (..), MD5 (..))
 
+import Benchmark
 import Charset
 import Bruteforce
 import Dictionary
@@ -64,17 +65,6 @@ userBruteforce args = do
                 content <- loadFile input             
                 print $ alg2bf alg charset minLen maxLen $ lines content
 
-benchmark :: IO()                
-benchmark = do
-    putStrLn "--= start =--"    
-    putStrLn "bruteforce MD5: one plaintext [4], plaintext len 1-4, charset: all printable ASCII, single core"
-    start <- getTime Monotonic
-    let result = bruteforce MD5 allPrintableASCIICharset 1 4 ["e5a4601548b3e753eb6a6a484af87c03"]
-    putStrLn $ show $ result
-    end <- getTime Monotonic
-    let timeInMillis = (fromIntegral $ toNanoSecs $ diffTimeSpec start end) / 1000000
-    putStrLn $ (show $ (((fromIntegral $ (keySpace (length allPrintableASCIICharset) 1 4)) / timeInMillis * 1000))) ++ " c/s"
-    putStrLn "--= stop =--"    
 
 dispatch :: [String] -> IO()
 dispatch args = do    
