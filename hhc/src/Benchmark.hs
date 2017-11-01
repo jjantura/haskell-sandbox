5bfeddac16866c6db83960d58a7d435a93b76633
Jarosław Jantura <jjantura@gmail.com>
2017-11-01 10:10:34 +0100

    Shorten MD5 benchmark duration

--- hhc/src/Benchmark.hs
+++ hhc/src/Benchmark.hs
@@ -10,12 +10,12 @@ import Crypto.Hash             (hashWith, HashAlgorithm(..), SHA1 (..), MD5 (..)
 benchmark :: IO()                
 benchmark = do
     putStrLn "--= start =--"    
-    putStrLn "bruteforce MD5: one plaintext [4], plaintext len 1-4, charset: all printable ASCII, single core"
+    putStrLn "bruteforce MD5: one plaintext [3], plaintext len 1-3, charset: all printable ASCII, single core"
     start <- getTime Monotonic
-    let result = bruteforce MD5 allPrintableASCIICharset 1 4 ["e5a4601548b3e753eb6a6a484af87c03"]
+    let result = bruteforce MD5 allPrintableASCIICharset 1 3 ["b55e74d4007b674b329d70f5550028ba"]
     print result
     end <- getTime Monotonic
     let timeInMillis = fromIntegral (toNanoSecs $ diffTimeSpec start end) / 1000000
-    putStrLn $ show (fromIntegral (keySpace (length allPrintableASCIICharset) 1 4) / timeInMillis * 1000) ++ " c/s"
+    putStrLn $ show (fromIntegral (keySpace (length allPrintableASCIICharset) 1 3) / timeInMillis * 1000) ++ " c/s"
     putStrLn "--= stop =--"    
     
