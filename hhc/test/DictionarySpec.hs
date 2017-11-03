f2a733fa83ead8dc3d3547265d64b2d18871cac6
Jarosław Jantura <jjantura@gmail.com>
2017-11-03 21:57:30 +0100

    Add dictionary method tests

--- hhc/test/DictionarySpec.hs
+++ hhc/test/DictionarySpec.hs
@@ -10,6 +10,8 @@ import Crypto.Hash (hashWith, SHA1 (..), MD5 (..))
 
 spec :: Spec
 spec = 
-    describe "SHA1.dictionary" $ 
+    describe "SHA1.dictionary" $ do
         it "cracks one letter SHA1 password with dictionary" $
             dictionary SHA1 ["a"] ["86f7e437faa5a7fce15d1ddcb9eaeaea377667b8"] `shouldBe` [("a", "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")]
+        it "cracks seven letter SHA1 password with dictionary" $
+            dictionary SHA1 ["aaaa", "1234567"] ["20eabe5d64b0e216796e834f52d61fd0b70332fc"] `shouldBe` [("1234567", "20eabe5d64b0e216796e834f52d61fd0b70332fc")]
