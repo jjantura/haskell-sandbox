fd25243ac058a553cfd87e6484106a9858d44ad6
Jarosław Jantura <jjantura@gmail.com>
2017-11-05 14:43:56 +0100

    Move bruteforce related function to proper file

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -23,45 +23,8 @@ import Bruteforce
 import Dictionary
 
 import File
+import CommandLine
 
--- TODO: this code is ugly and unreadable, replace with applicative optparse asap
-takeArgValue :: [String] -> String -> (Maybe String, Maybe String)
-takeArgValue args arg = 
-    let maybeArgIndex = arg `elemIndex` args in
-    let index = fromJust maybeArgIndex in
-    let argsCount = length args in   
-        if isJust maybeArgIndex then 
-            if index < argsCount then (Just arg, Just (args !! succ index)) else (Just arg, Nothing)
-        else (Nothing, Nothing)
-
-asInt :: Maybe String -> Int
-asInt s = if isJust s then read $ fromJust s :: Int else -1
-
-asString :: Maybe String -> String
-asString = fromMaybe ""
-
--- TODO: fix string to hash alg conversion
-alg2bf :: String -> String -> Int -> Int -> [String] -> [(String, String)]
-alg2bf alg = 
-    let lowercased = L.map toLower alg in        
-        case lowercased of
-            "sha1" -> bruteforce SHA1
-            _ -> bruteforce MD5
-
-argMap :: [String] -> Map (Maybe String) (Maybe String)
-argMap args = M.fromList $ L.map (takeArgValue args) ["-a", "-c", "-ll", "-ul", "-i"]
-
-userBruteforce :: [String] -> IO()
-userBruteforce args =             
-        let am = argMap args
-            alg = asString $ am ! Just "-a"
-            charset = asString $ am ! Just "-c"
-            minLen = asInt $ am ! Just "-ll"
-            maxLen = asInt $ am ! Just "-ul" 
-            input = asString $ am ! Just "-i" in
-            do 
-                content <- loadFile input             
-                print $ alg2bf alg charset minLen maxLen $ lines content
 
 
 dispatch :: [String] -> IO()
@@ -72,7 +35,7 @@ dispatch args =
             if isJust maybeMode then do
                 case fromJust maybeMode of 
                     "benchmark" -> benchmark
-                    "bruteforce" -> userBruteforce args
+                    "bruteforce" -> useBruteforce args
                     "dictionary" -> putStrLn "Not Implemented Yet"
                     "rules" -> putStrLn "Not Implemented Yet"
                     _ -> putStrLn "Unknown mode"
--- hhc/src/Bruteforce.hs
+++ hhc/src/Bruteforce.hs
@@ -1,23 +1,58 @@
 module Bruteforce
 
-    ( bruteforce
+    ( useBruteforce,
+      bruteforce
     ) where
 
-import           Crypto.Hash             (hashWith, HashAlgorithm (..))
-import           Data.List
+import           Crypto.Hash             (hashWith, HashAlgorithm (..), SHA1 (..), MD5 (..))
+import           Data.List as L 
 import           Data.Maybe
 import           Data.Text               (pack)
+import           Data.Char
 import           Data.Text.Encoding      (encodeUtf8)
 import           Charset
+import           Data.Map as M
+import           File
+import           CommandLine 
 
 
+-- TODO: fix string to hash alg conversion
+alg2bf :: String -> String -> Int -> Int -> [String] -> [(String, String)]
+alg2bf alg = 
+    let lowercased = L.map toLower alg in        
+        case lowercased of
+            "sha1" -> bruteforce SHA1
+            _ -> bruteforce MD5
+
+argMap :: [String] -> Map (Maybe String) (Maybe String)
+argMap args = M.fromList $ L.map (takeArgValue args) ["-a", "-c", "-ll", "-ul", "-i"]
+
+
+useBruteforce :: [String] -> IO()
+useBruteforce args =             
+        let am = argMap args
+            alg = asString $ am ! Just "-a"
+            charset = asString $ am ! Just "-c"
+            minLen = asInt $ am ! Just "-ll"
+            maxLen = asInt $ am ! Just "-ul" 
+            input = asString $ am ! Just "-i" in
+            do 
+                content <- loadFile input             
+                print $ alg2bf alg charset minLen maxLen $ lines content
+
+asInt :: Maybe String -> Int
+asInt s = if isJust s then read $ fromJust s :: Int else -1
+
+asString :: Maybe String -> String
+asString = fromMaybe ""
+                
 -- charset, min len, max len, cipher -> (hash, maybe plain text)
 bruteforce :: HashAlgorithm algorithm => algorithm -> String -> Int -> Int -> [String] -> [(String, String)]
 bruteforce algorithm charset minl maxl hashes =
   let
     plainToDigest e = show $ hashWith algorithm $ encodeUtf8 $ pack e
     digest e = plainToDigest $ genPlain charset e
-    foundIndices = foldl' (\a e -> if isJust (elemIndex (digest e) hashes) then e:a else a) [] [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]
-    plains = map (genPlain charset) foundIndices
+    foundIndices = L.foldl' (\a e -> if isJust (elemIndex (digest e) hashes) then e:a else a) [] [lowerLimit (length charset) minl .. upperLimit (length charset) maxl]
+    plains = L.map (genPlain charset) foundIndices
   in
-    zip plains $ map plainToDigest plains
+    zip plains $ L.map plainToDigest plains
