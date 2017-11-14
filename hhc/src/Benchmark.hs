73971e7da0e8ac34a3cdd198cff9ba6181e69d00
Jarosław Jantura <jjantura@gmail.com>
2017-11-14 21:55:42 +0100

    Improve benchmark formatting

--- hhc/src/Benchmark.hs
+++ hhc/src/Benchmark.hs
@@ -9,14 +9,17 @@ import           Crypto.Hash     (HashAlgorithm (..), MD5 (..), SHA1 (..),
                                   hashWith)
 import           System.Clock
 
+headers :: [String]
+headers = ["method", "algorithm", "hashes count", "keyspace", "thread count", "performance[c/s]", "result"]
+
 benchmark :: IO()
 benchmark = do
-    putStrLn "method,algorithm,hashes count,keyspace,thread count,performance,result"
+    putStrLn $ unwords $ map (++ ", ") headers
     start <- getTime Monotonic
     let result = bruteforce MD5 Cset.allPrintableASCIICharset 1 3 ["b55e74d4007b674b329d70f5550028ba"]
     end <- result `deepseq` getTime Monotonic
     let timeInMillis = fromIntegral (toNanoSecs $ diffTimeSpec start end) / 1000000
     let keySpace = Cset.keySpace (length Cset.allPrintableASCIICharset) 1 3
     let performance = fromIntegral keySpace / timeInMillis * 1000
-    putStrLn $ "bruteforce,MD5,1," ++ show keySpace ++ ",1," ++ show performance ++ " c/s,"
+    putStrLn $ "bruteforce, MD5, 1, " ++ show keySpace ++ ", 1, " ++ show performance 
 
