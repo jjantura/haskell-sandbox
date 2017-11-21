6e563728422a69d85ffc4a11263b419971f6752f
Jarosław Jantura <jjantura@gmail.com>
2017-11-21 19:58:07 +0100

    Add more tests for rules

--- hhc/test/RulesSpec.hs
+++ hhc/test/RulesSpec.hs
@@ -5,7 +5,9 @@ import           Test.Hspec
 import           Test.QuickCheck
     
 spec :: Spec
-spec = 
+spec = do
     describe "rules" $ do
         it "duplicates string" $ _duplicate "Cat" `shouldBe` "CatCat"
         it "reverses string" $ _reverse "Cat" `shouldBe` "taC"
+        it "capitalizes string" $ _capitalize "cat" `shouldBe` "CAT"
+        it "lowercases string" $ _lowercase "CAT" `shouldBe` "cat"
