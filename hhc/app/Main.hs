1330b07521b493aaa8d4d29b0b656938a236ccf9
Jarosław Jantura <jjantura@gmail.com>
2017-11-23 18:34:13 +0100

    Remove redundant imports from Main

--- hhc/app/Main.hs
+++ hhc/app/Main.hs
@@ -2,31 +2,15 @@
 
 module Main where
 
-import           Data.Char
-import           Data.List          as L
-import           Data.Map           as M
 import           Data.Maybe
-import           Data.Tuple.Select
-import           System.Clock
-import           System.Directory
 import           System.Environment
 import           System.Exit
-import           System.IO
-
-import           Control.Exception
-import           Control.Monad
-import           Crypto.Hash        (HashAlgorithm (..), MD5 (..), SHA1 (..),
-                                     hashWith)
 
 import           Benchmark
 import           Bruteforce
-import           Charset
 import           Dictionary
 
 import           CommandLine
-import           File
-
-
 
 dispatch :: [String] -> IO()
 dispatch args =
