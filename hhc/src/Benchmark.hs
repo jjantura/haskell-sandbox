46b8feff42cf0caa64f6e6448fde03592483ae3c
Jaroslaw Jantura <jjantura@gmail.com>
2017-11-26 17:41:42 +0100

    Remove magick numbers

--- hhc/src/Benchmark.hs
+++ hhc/src/Benchmark.hs
@@ -11,14 +11,17 @@ import           System.Clock
 headers :: [String]
 headers = ["method", "algorithm", "hashes count", "keyspace", "thread count", "performance[c/s]", "result"]
 
+nanoSecondsInSecond = 1000000
+milliSecondsInSecond = 1000
+
 benchmarkEntry :: String -> String -> [(String, String)] -> IO()
 benchmarkEntry method algo res = do
     start <- getTime Monotonic
     let result = res
     end <- result `deepseq` getTime Monotonic
-    let timeInMillis = fromIntegral (toNanoSecs $ diffTimeSpec start end) / 1000000
+    let timeInMillis = fromIntegral (toNanoSecs $ diffTimeSpec start end) / nanoSecondsInSecond
     let keySpace = Cset.keySpace (length Cset.allPrintableASCIICharset) 1 3
-    let performance = fromIntegral keySpace / timeInMillis * 1000
+    let performance = fromIntegral keySpace / timeInMillis * milliSecondsInSecond
     putStrLn $ method ++ ", " ++ algo ++ ", 1, " ++ show keySpace ++ ", 1, " ++ show performance
     
 benchmark :: IO()
