e78d46d92e58bc7c5107b35bfc77a30db8d2670b
Jarosław Jantura <jjantura@gmail.com>
2017-12-05 20:30:42 +0100

    Add sample + dependency

--- crud/app/Main.hs
+++ crud/app/Main.hs
@@ -1,6 +1,16 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+import Database.SQLite.Simple
 
-import Lib
+data CrudField = TestField Int String deriving (Show)
+
+instance FromRow CrudField where
+  fromRow = TestField <$> field <*> field
 
 main :: IO ()
-main = someFunc
+main = do
+  conn <- open "crud.db"
+  execute conn "INSERT INTO crud (str) VALUES (?)"
+    (Only ("test string 2" :: String))
+  r <- query_ conn "SELECT * from crud" :: IO [CrudField]
+  mapM_ print r
+  close conn
