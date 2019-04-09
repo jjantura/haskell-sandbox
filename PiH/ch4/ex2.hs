76d973b6146edb0056fc02c165ed81cc70976e1b
Jaroslaw Jantura <jjantura@gmail.com>
2019-04-09 22:04:52 +0200

    Add solution 3 for exercise 2 / chapter 4

--- PiH/ch4/ex2.hs
+++ PiH/ch4/ex2.hs
@@ -4,3 +4,8 @@ third xs = if length xs > 2 then Just (head $ drop 2 xs) else Nothing
 third' :: [a] -> Maybe a
 third' xs = if length xs > 2 then Just (xs !! 2) else Nothing
 
+third'' :: [a] -> Maybe a
+third'' [] = Nothing
+third'' [x] = Nothing
+third'' [x,y] = Nothing
+third'' (x:y:xs) = Just $ head xs
